008b754e19461f62e52e87993c20c8ffcd1af4d3
John Smith <doctorblue.king@gmail.com>
2020-06-06 08:19:19 +0700

    Update KotlinCoroutinesExample

---
+++ KotlinCoroutinesExample/app/src/main/java/com/example/kotlincoroutinesexample/coroutinecontext/TestTimeOut.kt
@@ -0,0 +1,26 @@
+package com.example.kotlincoroutinesexample.coroutinecontext
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+
+fun main() {
+    runBlocking {
+      /*  withTimeout(1800L){
+            repeat(1000){
+                println("I'm sleeping $it")
+                delay(500L)
+            }
+        }*/
+        val result=withTimeoutOrNull(1800L){
+            repeat(2){
+                println("I'm sleeping $it")
+                delay(500L)
+            }
+            "Done"
+        }
+        println("Result = $result")
+
+    }
+}

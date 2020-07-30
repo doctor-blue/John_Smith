6b1e5064ff19b823c0935f3308f890f87b6e8273
John Smith <doctorblue.king@gmail.com>
2020-07-30 16:31:51 +0700

    Update Note App

---
+++ NoteApp/app/src/main/java/com/example/noteapp/activities/AddNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.noteapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
